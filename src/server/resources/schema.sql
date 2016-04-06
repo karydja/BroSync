1a44fd8604bc863e8e68214fd2097fb92f61b190
karydja <karydjasouza@outlook.com>
2016-04-06 08:26:50 -0300

    Update SQL schema

--- src/server/resources/schema.sql
+++ src/server/resources/schema.sql
@@ -1,22 +1,21 @@
 CREATE TABLE users (
-   id       SERIAL PRIMARY KEY,
-   username VARCHAR UNIQUE NOT NULL,
-   email    VARCHAR UNIQUE NOT NULL,
-   password VARCHAR NOT NULL
+   id                SERIAL PRIMARY KEY,
+   username          VARCHAR UNIQUE NOT NULL,
+   email             VARCHAR UNIQUE NOT NULL,
+   password          VARCHAR NOT NULL,
+   brostorage_path   VARCHAR NOT NULL
 );
 
 CREATE TABLE files (
    id                                  SERIAL PRIMARY KEY,
    original_dir                        VARCHAR,
    original_path_within_original_dir   VARCHAR NOT NULL,
-   newest_timestamp                    TIMESTAMP,
-   filename                            VARCHAR NOT NULL
+   newest_timestamp                    TIMESTAMP
 );
 
 CREATE TABLE sharings (
    id                SERIAL PRIMARY KEY,
    user_id           INTEGER NOT NULL REFERENCES users(id),
    file_id           INTEGER NOT NULL REFERENCES files(id),
-   dir_path_for_user VARCHAR,
    path_for_user     VARCHAR
 );
